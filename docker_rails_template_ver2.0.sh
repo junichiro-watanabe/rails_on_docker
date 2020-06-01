dc678d81749cd3b917475bee829ce918405d0573
Your Name <junichiro.watanabe.1113@gmail.com>
2020-06-01 23:23:54 +0900

    Edit db_data volume

--- docker_rails_template_ver2.0.sh
+++ docker_rails_template_ver2.0.sh
@@ -145,6 +145,8 @@ EOF
 echo "make docker-compose.yml"
 cat << EOF > docker-compose.yml
 version: '3'
+volumes:
+  db_data:
 
 services:
   db:
@@ -153,7 +155,7 @@ services:
     environment:
       MYSQL_ROOT_PASSWORD: password
     volumes:
-      - ./db_data:/var/lib/mysql
+      - db_data:/var/lib/mysql
     ports:
       - 3306:3306
 
@@ -233,6 +235,67 @@ docker-compose run app rails new . --force --database=mysql --bundle-skip
 echo "sudo chown -R ${USER}:${GROUP} src docker docker-compose.yml"
 sudo chown -R ${USER}:${GROUP} src docker docker-compose.yml
 
+# Gemfile を編集
+echo "edit Gemfile"
+cat << EOF > src/Gemfile
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.6.6'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 6.0.2'
+# Use mysql as the database for Active Record
+gem 'mysql2', '>= 0.4.4'
+# Use Puma as the app server
+gem 'puma', '~> 4.1'
+# Use SCSS for stylesheets
+gem 'sass-rails', '>= 6'
+# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
+gem 'webpacker', '~> 4.0'
+# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+gem 'turbolinks', '~> 5'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.7'
+# Use Redis adapter to run Action Cable in production
+# gem 'redis', '~> 4.0'
+# Use Active Model has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use Active Storage variant
+# gem 'image_processing', '~> 1.2'
+
+# Reduces boot times through caching; required in config/boot.rb
+gem 'bootsnap', '>= 1.4.2', require: false
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+end
+
+group :development do
+  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  # Easy installation and use of web drivers to run system tests with browsers
+  gem 'webdrivers'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+EOF
+
 # database.yml を編集
 echo "edit database.yml"
 cat src/config/database.yml |
@@ -299,16 +362,9 @@ yarn-debug.log*
 .yarn-integrity
 EOF
 
-echo "edit db_data/.gitignore"
-cat << EOF > db_data/.gitignore
-/#innodb_temp
-EOF
-
 # ファイルの権限変更(2回目)
 echo "sudo chown -R ${USER}:${GROUP} src docker docker-compose.yml"
 sudo chown -R ${USER}:${GROUP} src docker docker-compose.yml
-echo "sudo chown -R 999:999 db_data"
-sudo chown -R 999:999 db_data
 
 echo "----- container start -----"
 # イメージをビルド
